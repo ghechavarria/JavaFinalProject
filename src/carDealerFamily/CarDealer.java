afa729df95d9d19229007c099295dcab4708f1b7
ghechavarria <49539243+ghechavarria@users.noreply.github.com>
2021-04-27 00:03:12 -0400

    Customer functionality and redesign

--- src/carDealerFamily/CarDealer.java
+++ src/carDealerFamily/CarDealer.java
@@ -13,7 +13,7 @@ import javax.swing.ImageIcon;
 
 public class CarDealer extends JFrame{
 	//GUI components declaration as fields
-	private ImageIcon imageUser = new ImageIcon("userImage.png");
+	private ImageIcon imageUser = new ImageIcon("userImage2.png");
 	private ImageIcon carPicture = new ImageIcon("car.png");
 	private JLabel picLabel;
 	private JLabel carLabel;
@@ -39,46 +39,46 @@ public class CarDealer extends JFrame{
 		//inside left panel
 		picLabel = new JLabel("");
 		carLabel = new JLabel("");
-		fNameLabel = new JLabel("First Name");
+		fNameLabel = new JLabel(" First Name");
 		fName = new JTextField();
-		lNameLabel = new JLabel("Last Name");
+		lNameLabel = new JLabel(" Last Name");
 		lName = new JTextField();
-		addressLabel = new JLabel("Address");
+		addressLabel = new JLabel(" Address");
 		address = new JTextField();
-		cityLabel = new JLabel("City");	
+		cityLabel = new JLabel(" City");	
 		city = new JTextField();
-		stateLabel = new JLabel("State");
+		stateLabel = new JLabel(" State");
 		state = new JTextField();
-		zipLabel = new JLabel("ZIP");
+		zipLabel = new JLabel(" ZIP");
 		zip = new JTextField();
-		countryLabel = new JLabel("Country");
+		countryLabel = new JLabel(" Country");
 		countryDropDown = new JComboBox(country);
 		signUpBtn = new JButton("Sign Up");
 		
 		//inside middle panel
 		bgModels = new ButtonGroup();
-		rbS40 = new JRadioButton("S40");
-		rbS60 = new JRadioButton("S60");
-		rbS70 = new JRadioButton("S70");
-		rbS80 = new JRadioButton("S80");
+		rbS40 = new JRadioButton("S40  ($27,700)");
+		rbS60 = new JRadioButton("S60  ($32,500)");
+		rbS70 = new JRadioButton("S70  ($36,000)");
+		rbS80 = new JRadioButton("S80  ($44,000)");
 		
-		packageA = new JCheckBox("Package 'A'");
-		packageB = new JCheckBox("Package 'B'");
-		metallicPaint = new JCheckBox("Metallic Paint");
+		packageA = new JCheckBox("Package A  (+$2,200)");
+		packageB = new JCheckBox("Package B  (+$3,250)");
+		metallicPaint = new JCheckBox("Metallic Paint  (+$650)");
 		
 		discountsLabel = new JLabel("Discounts");
 		tradeIn = new JCheckBox("Trade-In");
-		financing = new JCheckBox("Financing");
-		payCash = new JCheckBox("Pay Cash");
+		financing = new JCheckBox("Financing  (-7%)");
+		payCash = new JCheckBox("Pay Cash  (-$750)");
 		
 		//inside right panel
-		customerID = new JLabel("Customer ID: ");
+		customerID = new JLabel(" Customer ID: ");
 		idDisplay = new JLabel("------");
-		customerName = new JLabel("Customer Name: ");
+		customerName = new JLabel(" Customer Name: ");
 		nameDisplay = new JLabel("------  --------");
-		subtotal = new JLabel("Subtotal: ");
+		subtotal = new JLabel(" Subtotal: ");
 		subtotalDisplay = new JLabel("$-----.00");
-		total = new JLabel("Total: ");
+		total = new JLabel(" Total: ");
 		totalDisplay = new JLabel("$-----.00");
 		calculateBtn = new JButton("Calculate");
 		exitBtn = new JButton("Exit");
@@ -116,23 +116,31 @@ public class CarDealer extends JFrame{
 		JPanel rpanel3 = new JPanel();
 		
 		//add colors
-		signUpBtn.setBackground(Color.decode("#444B54"));
-		calculateBtn.setBackground(Color.decode("#444B54"));
-		exitBtn.setBackground(Color.decode("#444B54"));
+		signUpBtn.setBackground(Color.decode("#7B7C3C"));
+		calculateBtn.setBackground(Color.decode("#7B7C3C"));
+		exitBtn.setBackground(Color.decode("#7B7C3C"));
 		signUpBtn.setForeground(Color.WHITE);
 		calculateBtn.setForeground(Color.WHITE);
 		exitBtn.setForeground(Color.WHITE);
+		
+		signUpBtn.setBorderPainted(false);
+		calculateBtn.setBorderPainted(false);
+		exitBtn.setBorderPainted(false);
+		signUpBtn.setBorder(BorderFactory.createEmptyBorder(10,35,10,35));
+		calculateBtn.setBorder(BorderFactory.createEmptyBorder(10,25,10,25));
+		exitBtn.setBorder(BorderFactory.createEmptyBorder(10,45,10,45));
+		
 		leftPanel.setBackground(Color.decode("#E4E5E7"));
-		rightPanel.setBackground(Color.decode("#D8DABE"));
+		rightPanel.setBackground(Color.decode("#f5f5f5"));
 		lpanel1.setBackground(Color.decode("#E4E5E7"));
 		lpanel2.setBackground(Color.decode("#E4E5E7"));
 		lpanel3.setBackground(Color.decode("#E4E5E7"));
-		mpanel1.setBackground(Color.decode("#D8DABE"));
-		mpanel2.setBackground(Color.decode("#D8DABE"));
-		mpanel3.setBackground(Color.decode("#D8DABE"));
-		rpanel1.setBackground(Color.decode("#E4E5E7"));
-		rpanel2.setBackground(Color.decode("#E4E5E7"));
-		rpanel3.setBackground(Color.decode("#E4E5E7"));
+		mpanel1.setBackground(Color.decode("#f5f5f5"));
+		mpanel2.setBackground(Color.decode("#f5f5f5"));
+		mpanel3.setBackground(Color.decode("#f5f5f5"));
+		rpanel1.setBackground(Color.decode("#f5f5f5"));
+		rpanel2.setBackground(Color.decode("#f5f5f5"));
+		rpanel3.setBackground(Color.decode("#f5f5f5"));
 		
 		fNameLabel.setOpaque(true);
 		lNameLabel.setOpaque(true);
@@ -155,28 +163,28 @@ public class CarDealer extends JFrame{
 		rbS70.setOpaque(true);
 		rbS80.setOpaque(true);
 		
-		rbS40.setBackground(Color.decode("#E4E5E7"));
-		rbS60.setBackground(Color.decode("#E4E5E7"));
-		rbS70.setBackground(Color.decode("#E4E5E7"));
-		rbS80.setBackground(Color.decode("#E4E5E7"));
+		rbS40.setBackground(Color.decode("#f5f5f5"));
+		rbS60.setBackground(Color.decode("#f5f5f5"));
+		rbS70.setBackground(Color.decode("#f5f5f5"));
+		rbS80.setBackground(Color.decode("#f5f5f5"));
 		
 		packageA.setOpaque(true);
 		packageB.setOpaque(true);
 		metallicPaint.setOpaque(true);
 		
-		packageA.setBackground(Color.decode("#E4E5E7"));
-		packageB.setBackground(Color.decode("#E4E5E7"));
-		metallicPaint.setBackground(Color.decode("#E4E5E7"));
+		packageA.setBackground(Color.decode("#f5f5f5"));
+		packageB.setBackground(Color.decode("#f5f5f5"));
+		metallicPaint.setBackground(Color.decode("#f5f5f5"));
 		
 		discountsLabel.setOpaque(true);
 		tradeIn.setOpaque(true);
 		financing.setOpaque(true);
 		payCash.setOpaque(true);
 		
-		discountsLabel.setBackground(Color.decode("#E4E5E7"));
-		tradeIn.setBackground(Color.decode("#E4E5E7"));
-		financing.setBackground(Color.decode("#E4E5E7"));
-		payCash.setBackground(Color.decode("#E4E5E7"));
+		discountsLabel.setBackground(Color.decode("#f5f5f5"));
+		tradeIn.setBackground(Color.decode("#f5f5f5"));
+		financing.setBackground(Color.decode("#f5f5f5"));
+		payCash.setBackground(Color.decode("#f5f5f5"));
 		
 		customerID.setOpaque(true);
 		idDisplay.setOpaque(true);
@@ -187,27 +195,31 @@ public class CarDealer extends JFrame{
 		total.setOpaque(true);
 		totalDisplay.setOpaque(true);
 		
-		customerID.setBackground(Color.decode("#E4E5E7"));
-		idDisplay.setBackground(Color.decode("#E4E5E7"));
-		customerName.setBackground(Color.decode("#E4E5E7"));
-		nameDisplay.setBackground(Color.decode("#E4E5E7"));
-		subtotal.setBackground(Color.decode("#E4E5E7"));
-		subtotalDisplay.setBackground(Color.decode("#E4E5E7"));
-		total.setBackground(Color.decode("#E4E5E7"));
-		totalDisplay.setBackground(Color.decode("#E4E5E7"));
+		customerID.setBackground(Color.decode("#f5f5f5"));
+		idDisplay.setBackground(Color.decode("#f5f5f5"));
+		customerName.setBackground(Color.decode("#f5f5f5"));
+		nameDisplay.setBackground(Color.decode("#f5f5f5"));
+		subtotal.setBackground(Color.decode("#f5f5f5"));
+		subtotalDisplay.setBackground(Color.decode("#f5f5f5"));
+		total.setBackground(Color.decode("#f5f5f5"));
+		totalDisplay.setBackground(Color.decode("#f5f5f5"));
 		
 		//add layout manager
 		//create layout for sub-panels
-		leftPanel.setBounds(0, 0, 250, 550);
+		leftPanel.setBounds(0, 0, 300, 550);
 		leftPanel.setLayout(new GridLayout(3, 1)); //3 rows, 1 column
-		leftPanel.setBorder(BorderFactory.createTitledBorder("Customer"));
+		leftPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.decode("#A5AA6A"), 2, true),"Customer"));
+		((javax.swing.border.TitledBorder)leftPanel.getBorder()).setTitleColor(Color.decode("#444B54"));
+		((javax.swing.border.TitledBorder)leftPanel.getBorder()).setTitleFont(new Font("MonoLisa", Font.BOLD, 18));
 		
-		middlePanel.setBounds(300, 0, 250, 550);
+		middlePanel.setBounds(300, 0, 300, 550);
 		middlePanel.setLayout(new GridLayout(3, 1)); //3 rows, 1 column
 		
-		rightPanel.setBounds(600, 0, 250, 550);
+		rightPanel.setBounds(600, 0, 300, 550);
 		rightPanel.setLayout(new GridLayout(3, 1)); //3 rows, 1 column
-		rightPanel.setBorder(BorderFactory.createTitledBorder("Receipt"));
+		rightPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.decode("#A5AA6A"), 2, true),"Receipt"));
+		((javax.swing.border.TitledBorder)rightPanel.getBorder()).setTitleColor(Color.decode("#444B54"));
+		((javax.swing.border.TitledBorder)rightPanel.getBorder()).setTitleFont(new Font("MonoLisa", Font.BOLD, 18));
 		
 		//create panel for Image
 		leftPanel.add(lpanel1);
@@ -254,7 +266,9 @@ public class CarDealer extends JFrame{
 		//set layout for radio buttons
 		mpanel1.setLayout(new GridLayout(4,1));
 		//create border for Model panel
-		mpanel1.setBorder(BorderFactory.createTitledBorder("Models"));
+		mpanel1.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.decode("#A5AA6A"), 2, true),"Models"));
+		((javax.swing.border.TitledBorder)mpanel1.getBorder()).setTitleColor(Color.decode("#444B54"));
+		((javax.swing.border.TitledBorder)mpanel1.getBorder()).setTitleFont(new Font("MonoLisa", Font.BOLD, 18));
 		
 		//create panel for Extras
 		middlePanel.add(mpanel2);
@@ -265,7 +279,9 @@ public class CarDealer extends JFrame{
 		//set layout for check boxes
 		mpanel2.setLayout(new GridLayout(3,1));
 		//create border for Extras panel
-		mpanel2.setBorder(BorderFactory.createTitledBorder("Extras"));
+		mpanel2.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.decode("#A5AA6A"), 2, true),"Extras"));
+		((javax.swing.border.TitledBorder)mpanel2.getBorder()).setTitleColor(Color.decode("#444B54"));
+		((javax.swing.border.TitledBorder)mpanel2.getBorder()).setTitleFont(new Font("MonoLisa", Font.BOLD, 18));
 		
 		//create panel for Discounts
 		middlePanel.add(mpanel3);
@@ -276,7 +292,9 @@ public class CarDealer extends JFrame{
 		//set layout for check boxes
 		mpanel3.setLayout(new GridLayout(3,1));
 		//create border for Discounts panel
-		mpanel3.setBorder(BorderFactory.createTitledBorder("Discounts"));
+		mpanel3.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.decode("#A5AA6A"), 2, true),"Discounts"));
+		((javax.swing.border.TitledBorder)mpanel3.getBorder()).setTitleColor(Color.decode("#444B54"));
+		((javax.swing.border.TitledBorder)mpanel3.getBorder()).setTitleFont(new Font("MonoLisa", Font.BOLD, 18));
 		
 		//create panel for Receipt
 		rightPanel.add(rpanel1);
@@ -309,7 +327,7 @@ public class CarDealer extends JFrame{
 		//add to window
 		//set window parameters
 		JFrame frame = new JFrame();
-		frame.setSize(865, 590);
+		frame.setSize(917, 590);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
@@ -321,24 +339,54 @@ public class CarDealer extends JFrame{
 		
 		//SIGN UP ACTION LISTENER
 		signUpBtn.addActionListener(e->{
+			//button audio
 			playAudio("sound.wav");
-
+			//get input data
+			//fName, lName, address, city, state, zip
+			String fnValue, lnValue, aValue, cValue, sValue, zValue, ctrValue;
+			fnValue = lnValue = aValue = cValue = sValue = zValue = ctrValue = "";
+			fnValue = fName.getText();
+			lnValue = lName.getText();
+			aValue = address.getText();
+			cValue = city.getText();
+			sValue = state.getText();
+			zValue = zip.getText();
+			ctrValue = countryDropDown.getSelectedItem().toString();
+			
+			//test fields
+			//System.out.println(fnValue + " " + lnValue + " " + aValue + " " + cValue + " " + sValue + " " + zValue + " " + ctrValue);
+			
+			//if all values have been inputted
+			if (!fnValue.isEmpty() && !lnValue.isEmpty() && !aValue.isEmpty() && !cValue.isEmpty() && !sValue.isEmpty() && !zValue.isEmpty()) {
+				//create new Customer object
+				Customer customer = new Customer(fnValue, lnValue, aValue, cValue, sValue, zValue, ctrValue);
+				//display customer id and name in receipt section
+				idDisplay.setText(customer.getID());
+				nameDisplay.setText(customer.getfName() + " " + customer.getlName());
+			} else {
+				JOptionPane.showMessageDialog(null, "Input Error...Fill in all Customer fields.", "Warning", 2);
+			}
+			
 		});
 		
 		//CALCULATE ACTION LISTENER
 		calculateBtn.addActionListener(e->{
+			//button audio
 			playAudio("sound.wav");
+			
 
 		});
 		
 		//EXIT ACTION LISTENER
 		exitBtn.addActionListener(e->{
+			//button audio
 			playAudio("shutdown.wav");
 			try {
 				Thread.sleep(1500);
 			} catch (InterruptedException e1) {
-				JOptionPane.showMessageDialog(null, "Exit Error");
+				JOptionPane.showMessageDialog(null, "Exit Error", "Error", 2);
 			}
+			//exit java program
 			System.exit(0);
 
 		});
@@ -423,7 +471,7 @@ public class CarDealer extends JFrame{
 	}
 
 	public static void main(String[] args) {
-		setUIFont (new javax.swing.plaf.FontUIResource("MonoLisa",Font.PLAIN,15));
+		setUIFont (new javax.swing.plaf.FontUIResource("MonoLisa",Font.PLAIN,16));
 		createLoadBar();
 		new CarDealer(); /*GUI*/
 		Customer c = new Customer("Luis", "Lastname", "0000 NW 000th Ct", "Davie", "FL", "00000", "United States");
@@ -444,7 +492,7 @@ public class CarDealer extends JFrame{
             clip.start();
         }
         catch(Exception e) {
-            JOptionPane.showMessageDialog(null, "Music File Error");
+            JOptionPane.showMessageDialog(null, "Music File Error", "Error", 2);
         }
     }//end of playAudio
 	
